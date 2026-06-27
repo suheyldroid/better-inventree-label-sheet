50a26f604fbfeb17e17ae3999fcdcb326053a9a2
suheyl <64775466+suheyldroid@users.noreply.github.com>
2026-06-28 00:18:34 +0300

    feat: add A4 2x6 full bleed layout (no margins or spacing)

--- better_label_printer/layouts.py
+++ better_label_printer/layouts.py
@@ -8,30 +8,29 @@ List of sheet label paper layouts
 """
 
 import dataclasses
-
 from typing import Optional
 
 
 @dataclasses.dataclass
 class PaperSize:
-    display_name: str   # paper size display name (e.g. "A4")
-    width: float        # mm
-    height: float       # mm
+    display_name: str  # paper size display name (e.g. "A4")
+    width: float  # mm
+    height: float  # mm
 
 
 @dataclasses.dataclass
 class SheetLayout:
     display_name: str
     page_size: PaperSize
-    label_width: float      # mm
-    label_height: float     # mm
+    label_width: float  # mm
+    label_height: float  # mm
     columns: int
     rows: int
-    column_spacing: float   # mm
-    row_spacing: float      # mm
-    corner_radius: float    # mm, 0 means sharp corners
-    spacing_top: Optional[float] = None    # None means automatic centering
-    spacing_left: Optional[float] = None   # None means automatic centering
+    column_spacing: float  # mm
+    row_spacing: float  # mm
+    corner_radius: float  # mm, 0 means sharp corners
+    spacing_top: Optional[float] = None  # None means automatic centering
+    spacing_left: Optional[float] = None  # None means automatic centering
 
     @property
     def cells(self) -> int:
@@ -43,23 +42,28 @@ class SheetLayout:
             return self.spacing_top
         else:
             return (
-                self.page_size.height - (self.label_height * self.rows + self.row_spacing * (self.rows - 1))
+                self.page_size.height
+                - (self.label_height * self.rows + self.row_spacing * (self.rows - 1))
             ) / 2
-    
+
     def row_position_top(self, row: int) -> float:
         """
         returns the distance from the top of the page
         to the top of the specified row (starting with 0)
         """
         return self.spacing_top_computed + row * (self.label_height + self.row_spacing)
-        
+
     @property
     def spacing_left_computed(self) -> float:
         if self.spacing_left is not None:
             return self.spacing_left
         else:
             return (
-                self.page_size.width - (self.label_width * self.columns + self.column_spacing * (self.columns - 1))
+                self.page_size.width
+                - (
+                    self.label_width * self.columns
+                    + self.column_spacing * (self.columns - 1)
+                )
             ) / 2
 
     def column_position_left(self, column: int) -> float:
@@ -67,14 +71,17 @@ class SheetLayout:
         returns the distance from the left edge of the page
         to the left edge of the specified column (starting with 0)
         """
-        return self.spacing_left_computed + column * (self.label_width + self.column_spacing)
+        return self.spacing_left_computed + column * (
+            self.label_width + self.column_spacing
+        )
 
     def __str__(self) -> str:
         return f"{self.display_name} ({self.page_size.display_name}, {self.label_width}mm x {self.label_height}mm, {self.columns} columns x {self.rows} rows, {'round corners' if self.corner_radius != 0 else 'sharp corners'})"
 
+
 PAPER_SIZES = {
     "A4": PaperSize("A4", 210, 297),
-    "Letter": PaperSize("Letter", 215.9, 279.4)
+    "Letter": PaperSize("Letter", 215.9, 279.4),
 }
 
 LAYOUTS = {
@@ -89,7 +96,7 @@ LAYOUTS = {
         row_spacing=0,
         corner_radius=3,
         spacing_top=12.7,
-        spacing_left=4.6
+        spacing_left=4.6,
     ),
     "22805": SheetLayout(
         display_name="Avery 22805",
@@ -98,11 +105,11 @@ LAYOUTS = {
         label_height=37,
         columns=4,
         rows=6,
-        column_spacing=8.8,                     # space between two columns, not margins
-        row_spacing=6.1,                        # space between two rows, not margins
-        corner_radius=0,                        # radius of label corners
-        spacing_top=12,                      # Spacing of top margin
-        spacing_left=20.1                         # Spacing of left margin
+        column_spacing=8.8,  # space between two columns, not margins
+        row_spacing=6.1,  # space between two rows, not margins
+        corner_radius=0,  # radius of label corners
+        spacing_top=12,  # Spacing of top margin
+        spacing_left=20.1,  # Spacing of left margin
     ),
     "4780": SheetLayout(
         display_name="4780",
@@ -113,7 +120,7 @@ LAYOUTS = {
         rows=10,
         column_spacing=0,
         row_spacing=0,
-        corner_radius=0
+        corner_radius=0,
     ),
     "4737": SheetLayout(
         display_name="4737",
@@ -124,7 +131,7 @@ LAYOUTS = {
         rows=9,
         column_spacing=2.54,
         row_spacing=0,
-        corner_radius=3
+        corner_radius=3,
     ),
     "4201": SheetLayout(
         display_name="4201",
@@ -135,7 +142,7 @@ LAYOUTS = {
         rows=16,
         column_spacing=2.6,
         row_spacing=0,
-        corner_radius=2
+        corner_radius=2,
     ),
     "7120-25": SheetLayout(
         display_name="7120-25",
@@ -146,7 +153,7 @@ LAYOUTS = {
         rows=7,
         column_spacing=5,
         row_spacing=5,
-        corner_radius=0
+        corner_radius=0,
     ),
     "7160-10": SheetLayout(
         display_name="7160-10",
@@ -157,7 +164,7 @@ LAYOUTS = {
         rows=7,
         column_spacing=2.54,
         row_spacing=0,
-        corner_radius=2.54
+        corner_radius=2.54,
     ),
     "4360": SheetLayout(
         display_name="4360",
@@ -168,7 +175,7 @@ LAYOUTS = {
         rows=8,
         column_spacing=0,
         row_spacing=0,
-        corner_radius=0
+        corner_radius=0,
     ),
     "1367853": SheetLayout(
         display_name="1367853",
@@ -179,7 +186,7 @@ LAYOUTS = {
         rows=16,
         column_spacing=0,
         row_spacing=0,
-        corner_radius=0
+        corner_radius=0,
     ),
     "4210": SheetLayout(
         display_name="4210",
@@ -190,7 +197,7 @@ LAYOUTS = {
         rows=22,
         column_spacing=0,
         row_spacing=0,
-        corner_radius=0
+        corner_radius=0,
     ),
     "1367586": SheetLayout(
         display_name="1367586",
@@ -201,7 +208,7 @@ LAYOUTS = {
         rows=8,
         column_spacing=0,
         row_spacing=0,
-        corner_radius=0
+        corner_radius=0,
     ),
     "8724": SheetLayout(
         display_name="8724",
@@ -212,7 +219,7 @@ LAYOUTS = {
         rows=21,
         column_spacing=4.8,
         row_spacing=1.6,
-        corner_radius=1
+        corner_radius=1,
     ),
     "50x25-R": SheetLayout(
         display_name="Avery 50x25-R",
@@ -225,7 +232,7 @@ LAYOUTS = {
         row_spacing=10.0,
         corner_radius=2.0,
     ),
-    "105x42-R": SheetLayout (
+    "105x42-R": SheetLayout(
         display_name="Avery 105x42-R",
         page_size=PAPER_SIZES["A4"],
         label_width=105.0,
@@ -236,7 +243,7 @@ LAYOUTS = {
         row_spacing=0,
         corner_radius=0,
     ),
-    "40x12-R": SheetLayout (
+    "40x12-R": SheetLayout(
         display_name="Avery 40x12-R",
         page_size=PAPER_SIZES["A4"],
         label_width=40.0,
@@ -246,7 +253,7 @@ LAYOUTS = {
         column_spacing=5.0,
         row_spacing=5.0,
         corner_radius=2.0,
-    ),   
+    ),
     "8698": SheetLayout(
         display_name="8698",
         page_size=PAPER_SIZES["A4"],
@@ -256,18 +263,30 @@ LAYOUTS = {
         rows=10,
         column_spacing=0,
         row_spacing=0,
-        corner_radius=0
+        corner_radius=0,
+    ),
+    "A4-2x6-full": SheetLayout(
+        display_name="A4 2x6 Full Bleed",
+        page_size=PAPER_SIZES["A4"],
+        label_width=105.0,
+        label_height=49.5,
+        columns=2,
+        rows=6,
+        column_spacing=0,
+        row_spacing=0,
+        corner_radius=0,
+        spacing_top=0,
+        spacing_left=0,
     ),
 }
 
 LAYOUT_SELECT_OPTIONS = [
-    ("auto_round", "Auto (round) - Automatically detect correct layout for label template according to metadata or size (prefer round-corner labels)"),
-    ("auto_sharp", "Auto (sharp) - Automatically detect correct layout for label template according to metadata or size (prefer sharp-corner labels)")
-] + [
     (
-        code, 
-        str(layout)
-    ) 
-    for code, layout in LAYOUTS.items()
-]
-
+        "auto_round",
+        "Auto (round) - Automatically detect correct layout for label template according to metadata or size (prefer round-corner labels)",
+    ),
+    (
+        "auto_sharp",
+        "Auto (sharp) - Automatically detect correct layout for label template according to metadata or size (prefer sharp-corner labels)",
+    ),
+] + [(code, str(layout)) for code, layout in LAYOUTS.items()]
